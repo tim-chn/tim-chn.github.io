f3dfc39523a66f9e87a57abbbd664b66a2083de3
Timothy Chen <timothychen@timothys-mbp-2.mynetworksettings.com>
2024-12-13 10:57:29 -0800

    ux update

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const theme = localStorage.getItem('theme');
     const body = document.body;
     const themeIcon = document.querySelector('.theme-icon');
+    const resumeIcon = document.querySelector('.resume-icon'); // Select the resume icon
 
     if (theme === 'dark') {
         body.classList.add('dark-mode');
         themeIcon.src = 'moon-icon.png';
+        resumeIcon.src = 'resume-icon-dark.png'; // Dark mode resume icon
     } else {
         body.classList.remove('dark-mode');
         themeIcon.src = 'sun-icon.png';
+        resumeIcon.src = 'resume-icon.png'; // Light mode resume icon
     }
 
     document.getElementById('theme-toggle').addEventListener('click', function () {
         body.classList.toggle('dark-mode');
         
-        // Update icon and local storage
+        // Update icons and local storage
         if (body.classList.contains('dark-mode')) {
             themeIcon.src = 'moon-icon.png';
+            resumeIcon.src = 'resume-icon-dark.png'; // Dark mode resume icon
             localStorage.setItem('theme', 'dark');
         } else {
             themeIcon.src = 'sun-icon.png';
+            resumeIcon.src = 'resume-icon.png'; // Light mode resume icon
             localStorage.setItem('theme', 'light');
         }
     });
