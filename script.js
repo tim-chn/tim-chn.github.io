48c468d5ee33e4a7741039c030262346f7bf432f
Timothy Chen <timothychen@timothys-mbp-2.lan>
2025-05-31 18:46:04 -0700

    style updates

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const theme = localStorage.getItem('theme');
     const body = document.body;
     const themeIcon = document.querySelector('.theme-icon');
-    const resumeIcon = document.querySelector('.resume-icon'); // Select the resume icon
+    const resumeIcon = document.querySelector('.resume-icon'); 
 
     if (theme === 'dark') {
         body.classList.add('dark-mode');
         themeIcon.src = 'moon-icon.png';
-        resumeIcon.src = 'resume-icon-dark.png'; // Dark mode resume icon
+        resumeIcon.src = 'resume-icon-dark.png'; 
     } else {
         body.classList.remove('dark-mode');
         themeIcon.src = 'sun-icon.png';
-        resumeIcon.src = 'resume-icon.png'; // Light mode resume icon
-    }
+        resumeIcon.src = 'resume-icon.png'; 
 
     document.getElementById('theme-toggle').addEventListener('click', function () {
         body.classList.toggle('dark-mode');
@@ -21,11 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
         // Update icons and local storage
         if (body.classList.contains('dark-mode')) {
             themeIcon.src = 'moon-icon.png';
-            resumeIcon.src = 'resume-icon-dark.png'; // Dark mode resume icon
+            resumeIcon.src = 'resume-icon-dark.png'; 
             localStorage.setItem('theme', 'dark');
         } else {
             themeIcon.src = 'sun-icon.png';
-            resumeIcon.src = 'resume-icon.png'; // Light mode resume icon
+            resumeIcon.src = 'resume-icon.png';
             localStorage.setItem('theme', 'light');
         }
     });
